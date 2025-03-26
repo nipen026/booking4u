085bc6f915d53eda9d1b4dbf5952f901111d89f6
Nipen Khant <khantnipen@gmail.com>
2025-03-26 18:41:28 +0530

    added changes

--- src/components/AdminBookings/index.js
+++ src/components/AdminBookings/index.js
@@ -85,9 +85,9 @@ const AdminAllBookings = ({ bookingsData }) => {
                     </Typography>
                 </Box>
             ) : (
-                bookingsData.map((booking, index) => {
-                    const startTime = dayjs(booking.Slot.startTime, "HH:mm:ss");
-                    const endTime = dayjs(booking.Slot.endTime, "HH:mm:ss").add(1, "hour");
+                bookingsData.filter((obj)=>obj.status !== 'Cancelled').map((booking, index) => {
+                    const startTime = dayjs(booking?.Slot?.startTime, "HH:mm:ss");
+                    const endTime = dayjs(booking?.Slot?.endTime, "HH:mm:ss").add(1, "hour");
 
                     return (
                         <Card key={index} sx={{ mb: 2, p: 2 }}>
@@ -95,26 +95,26 @@ const AdminAllBookings = ({ bookingsData }) => {
                                 <Grid container spacing={2} alignItems="center">
                                     <Grid item xs={12} sm={6}>
                                         <Typography variant="h6" fontWeight="bold" align='left'>
-                                            {booking.Box.name}
+                                            {booking?.Box?.name}
                                         </Typography>
                                         <Typography sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
                                             <FaCalendarAlt style={{ marginRight: '8px' }} />
-                                            {dayjs(booking.Slot.date).format("DD MMM YYYY")}
+                                            {dayjs(booking?.Slot?.date).format("DD MMM YYYY")}
                                             <span style={{ fontWeight: 'bold', marginLeft: '10px' }}>
-                                                {startTime.format("hh:mm A")} - {endTime.format("hh:mm A")}
+                                                {startTime?.format("hh:mm A")} - {endTime?.format("hh:mm A")}
                                             </span>
                                         </Typography>
                                         <Typography variant="body2" color="text.secondary" mt={1} align='left'>
-                                            Booking ID: {booking.id}
+                                            Booking ID: {booking?.id}
                                         </Typography>
                                     </Grid>
 
                                     <Grid item xs={12} sm={6} display="flex" alignItems="center" justifyContent="flex-end" gap={1}>
                                         <Chip
-                                            label={booking.status}
+                                            label={booking?.status}
                                             sx={{
-                                                backgroundColor: booking.status === 'Confirmed' ? 'rgba(34, 197, 94, 0.1)' : 'rgba(239, 68, 68, 0.1)',
-                                                color: booking.status === 'Confirmed' ? 'green' : 'red',
+                                                backgroundColor: booking?.status === 'Confirmed' ? 'rgba(34, 197, 94, 0.1)' : 'rgba(239, 68, 68, 0.1)',
+                                                color: booking?.status === 'Confirmed' ? 'green' : 'red',
                                                 fontWeight: 'bold',
                                                 borderRadius: '6px'
                                             }}
@@ -135,31 +135,31 @@ const AdminAllBookings = ({ bookingsData }) => {
                 <Dialog open={openDialog} onClose={handleCloseDialog} fullWidth maxWidth="lg">
                     <DialogTitle>Booking Details</DialogTitle>
                     <DialogContent>
-                        <Typography variant="h6" mt={2} fontWeight="bold">{selectedBooking.Box.name}</Typography>
+                        <Typography variant="h6" mt={2} fontWeight="bold">{selectedBooking?.Box?.name}</Typography>
                         <Typography mt={2}>
                             <FaCalendarAlt style={{ marginRight: '8px' }} />
-                            {dayjs(selectedBooking.Slot.date).format("DD MMM YYYY")}
+                            {dayjs(selectedBooking?.Slot?.date).format("DD MMM YYYY")}
                         </Typography>
                         <Typography mt={2}>
-                            Check-in: <strong>{dayjs(selectedBooking.Slot.startTime, "HH:mm:ss").format("hh:mm A")}</strong>
+                            Check-in: <strong>{dayjs(selectedBooking?.Slot?.startTime, "HH:mm:ss").format("hh:mm A")}</strong>
                         </Typography>
                         <Typography mt={2}>
-                            Check-out: <strong>{dayjs(selectedBooking.Slot.endTime, "HH:mm:ss").add(1, "hour").format("hh:mm A")}</strong>
+                            Check-out: <strong>{dayjs(selectedBooking?.Slot?.endTime, "HH:mm:ss").add(1, "hour").format("hh:mm A")}</strong>
                         </Typography>
                         <Typography variant="body2" mt={2}>
-                            Booking ID: <strong>{selectedBooking.id}</strong>
+                            Booking ID: <strong>{selectedBooking?.id}</strong>
                         </Typography>
                         <Typography variant="body2" mt={2}>
-                            User Name: <strong>{selectedBooking.User.firstName} {selectedBooking.User.lastName}</strong>
+                            User Name: <strong>{selectedBooking?.User?.firstName} {selectedBooking?.User?.lastName}</strong>
                         </Typography>
                         <Typography variant="body2" mt={2}>
-                            Email: <strong>{selectedBooking.User?.email}</strong>
+                            Email: <strong>{selectedBooking?.User?.email}</strong>
                         </Typography>
                         <Typography variant="body2" mt={2}>
-                            Phone: <strong>{selectedBooking.User?.phone}</strong>
+                            Phone: <strong>{selectedBooking?.User?.phone}</strong>
                         </Typography>
                         <Typography variant="body2" mt={2}>
-                            Status: <strong>{selectedBooking.status}</strong>
+                            Status: <strong>{selectedBooking?.status}</strong>
                         </Typography>
                     </DialogContent>
                     <DialogActions>
